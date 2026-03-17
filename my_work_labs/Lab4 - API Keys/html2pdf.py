bb1f5accf23f82c72f4aa7d61a238f24060e1544
Stephen Kerr <kerrstephen17@gmail.com>
2026-03-17 23:32:38 +0000

    tried lab 4 but keep getting auth error from html2pdf

---
+++ my_work_labs/Lab4 - API Keys/html2pdf.py
@@ -0,0 +1,41 @@
+# in this program we will 
+# convert a website into a pdf
+# using and API key from html2pdf.com
+# Autherd by Stephen Kerr
+
+# imports
+import requests # for making the API request
+import urllib.parse # for encoding the url
+
+# api key from html2pdf.com
+api_key = "HNBXFTB3EBTdSG2m8H0rrolHZPJUZfXeYxYhhGzrezCCgyMzyY4CSrtb7qFC69mp"
+
+# target url to convert to pdf
+url = "https://andrewbeatty1.pythonanywhere.com/bookviewer.html"
+
+
+# API url for html2pdf.com
+api_url = "https://api.html2pdf.app/v1/generate"
+
+# parameters for the API request
+params = {
+    "url": url,
+    "api_key": api_key
+}
+
+# parse to encode the parameters for the API request
+encoded_params = urllib.parse.urlencode(params)
+
+# request url
+request_url = api_url + "?" + encoded_params
+
+# make the API request
+response = requests.get(request_url)
+
+# check if the request was successful
+print(response.status_code)
+print(response.text)
+
+# save the pdf to a file
+with open("sample.pdf", "wb") as pdf_file:
+    pdf_file.write(response.content)
