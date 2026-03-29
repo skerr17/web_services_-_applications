d398994d388406463374b1046500193efa22ca35
Stephen Kerr <kerrstephen17@gmail.com>
2026-03-29 16:26:06 +0100

    created sql table using python lab 6

--- my_work_labs/lab6 - python and databases/create_table.py
+++ my_work_labs/lab6 - python and databases/create_table.py
@@ -1,25 +1,31 @@
-# create a SQL databases 
+# creates a SQL table
 # Authored: Stephen Kerr
 
+# imports
 import mysql.connector
-
 from config import db_connection as cfg
 
 # create a connection to the database
 connection = mysql.connector.connect(
     host=cfg['host'],
     user=cfg['user'],
-    password=cfg['password']
+    password=cfg['password'],
+    database='wsaa'
 )
 
 # cursor to execute SQL commands
 cursor = connection.cursor()
 
-# create database
-cursor.execute("CREATE DATABASE wsaa")
+# create table
+cursor.execute("""CREATE TABLE books (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    title VARCHAR(255),
+    author VARCHAR(255)
+)""")
 
 # close the cursor
 cursor.close()
 
 # close the connection
 connection.close()
+
